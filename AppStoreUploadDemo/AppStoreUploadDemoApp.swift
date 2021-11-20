7169fc92de5bebdd27ca54f0e9eb681f988ae74a
Heiko von Wegerer <heiko.von-wegerer@deutschebahn.com>
2021-11-20 23:41:54 +0100

    Initial project setup (SwiftUI)

---
+++ AppStoreUploadDemo/AppStoreUploadDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreUploadDemoApp.swift
+//  AppStoreUploadDemo
+//
+//  Created by Heiko von Wegerer on 20.11.21.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreUploadDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
