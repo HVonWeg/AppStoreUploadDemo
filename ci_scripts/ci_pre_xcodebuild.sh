edff4bc31d8405793e03f141ec90bd96aa576592
Heiko von Wegerer <heiko.von-wegerer@deutschebahn.com>
2021-11-21 21:42:37 +0100

    Fix APP_ICON_PATH

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -6,13 +6,10 @@
 #  Created by Heiko von Wegerer on 21.11.21.
 #
 
-echo "workspace: '$CI_WORKSPACE'"
-echo "action: '$CI_XCODEBUILD_ACTION'"
 if [[ -n $CI_PULL_REQUEST_NUMBER && $CI_XCODEBUILD_ACTION == 'archive' ]];
 then
 echo "Setting Beta App Icon"
-    APP_ICON_PATH=$CI_WORKSPACE/Assets.xcassets/AppIcon.appiconset
-    echo "APP_ICON_PATH: '$APP_ICON_PATH'"
+    APP_ICON_PATH=$CI_WORKSPACE/AppStoreUploadDemo/Assets.xcassets/AppIcon.appiconset
     
     # Remove existing App Icon
     rm -rf $APP_ICON_PATH
